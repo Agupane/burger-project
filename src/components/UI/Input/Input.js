7defad77e9a42640b05f9304c52d2e2e8e8e7477
Agustin Pane <agustin.pane@gmail.com>
2018-12-14 17:36:52 -0300

    Input refactor

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const input = (props)=> {
+
+    let inputElement = null;
+    switch(props.inputtype){
+        case('input'):{
+            inputElement = <input className={classes.InputElement} {...props}/>;
+            break;
+        }
+        case('textarea'):{
+            inputElement = <textarea {...props} className={classes.InputElement}/>;
+            break;
+        }
+        default:{
+            inputElement = <input className={classes.InputElement} {...props}/>;
+            break;
+        }
+    }
+
+
+    return(
+    <div className={classes.Input}>
+        <label className={classes.Label}>{props.label}</label>
+        {inputElement}
+    </div>
+    );
+};
+
+export default input;
